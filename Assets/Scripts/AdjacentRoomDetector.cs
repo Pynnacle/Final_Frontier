aef5f7930285a53214dee626a0d2914430591337
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2020-02-20 22:12:30 -0500

    Add 5 room prefabs
    - Add 5 room prefabs for testing.

---
+++ Assets/Scripts/AdjacentRoomDetector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AdjacentRoomDetector : MonoBehaviour
+{
+    public float magnitude;
+    public Color safeColor;
+    public Color warningColor;
+    public Color hitColor;
+    public bool hit;
+    public float hitDistance;
+    public float hitPercentage;
+    public float sensitivity;
+
+    public string layerToAvoid;
+
+    void Update()
+    {
+
+        Debug.Log("Running running running");
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        int layerMask = 1 << LayerMask.NameToLayer(layerToAvoid);
+        //If you want to add more layers to search for, add them here^.
+
+        hit = Physics.Raycast(ray, out hitInfo, magnitude, layerMask);
+
+        if (hit)
+        {
+            hitDistance = hitInfo.distance;
+            hitPercentage = 1 - (hitDistance / magnitude);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.Lerp(warningColor, hitColor, hitPercentage));
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.forward * magnitude, safeColor);
+        }
+
+    }
+}
---
+++ Assets/Scripts/RoomOpening.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OpeningSide
+{
+    LEFT, RIGHT, TOP, BOTTOM
+}
+
+public class RoomOpening : MonoBehaviour
+{
+
+    public OpeningSide sideOpeningIsOn;
+    GameObject roomPicker;
+    Vector3 newRoomPosition;
+    bool alreadyCrossed; //This is a temporary fix since raycast doesn't want to cooperate.
+
+    void Start()
+    {
+        roomPicker = GameObject.Find("Random Room Picker");
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "Player" && !alreadyCrossed)
+        {
+            if (sideOpeningIsOn == OpeningSide.LEFT)
+            {
+
+                newRoomPosition = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y, transform.parent.transform.position.z - 10);
+                GameObject temp1 = Instantiate<GameObject>(roomPicker.GetComponent<RoomPicker>().PickRoom(), newRoomPosition, transform.parent.transform.rotation);
+
+            }
+            else if (sideOpeningIsOn == OpeningSide.RIGHT)
+            {
+
+                newRoomPosition = new Vector3(transform.parent.transform.position.x, transform.parent.transform.position.y, transform.parent.transform.position.z + 10);
+                GameObject temp2 = Instantiate<GameObject>(roomPicker.GetComponent<RoomPicker>().PickRoom(), newRoomPosition, transform.parent.transform.rotation);
+
+            }
+            else if (sideOpeningIsOn == OpeningSide.TOP)
+            {
+
+            }
+            else if (sideOpeningIsOn == OpeningSide.BOTTOM)
+            {
+
+            }
+
+            alreadyCrossed = true;
+        }
+    }
+}
