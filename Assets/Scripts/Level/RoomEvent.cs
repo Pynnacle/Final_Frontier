089b37325336f6d75365f96c941c451c16cf215d
DanthonyS <59703244+DanthonyS@users.noreply.github.com>
2020-03-28 17:15:35 -0400

    Add RoomEvent Scriptable Object
    - I created a scriptable object for room events. Each event has a name, description, and button text.
    - I also created a UI template for the popup.

---
+++ Assets/Scripts/Level/RoomEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Room Event", fileName = "New Room Event")]
+public class RoomEvent : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public string buttonText;
+}
